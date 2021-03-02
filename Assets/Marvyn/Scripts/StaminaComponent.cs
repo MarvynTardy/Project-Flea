4fddc41722a1cccf8c9a39f63477eb8fa6f99396
Marvyn Tardy <marvyn.tardy@e-artsup.net>
2021-03-02 15:34:41 +0100

    HDRP Test + Creation staminaComponent

---
+++ Assets/Marvyn/Scripts/StaminaComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaComponent : MonoBehaviour
+{
+    [SerializeField]
+    private float m_MaxStamina;
+    private float m_CurrentStamina;
+
+    public void UseStamina(float p_Amount)
+    {
+        m_CurrentStamina -= p_Amount;
+    }
+
+    public void GainStamina(float p_Amount)
+    {
+        m_CurrentStamina += p_Amount;
+    }
+}
