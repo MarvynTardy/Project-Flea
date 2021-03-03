af829a347be685cdcdb6fe73e5872d5fbd9683f9
Marvyn Tardy <marvyn.tardy@e-artsup.net>
2021-03-03 17:07:43 +0100

    StaminaUI + components

--- Assets/Marvyn/Scripts/StaminaComponent.cs
+++ Assets/Marvyn/Scripts/StaminaComponent.cs
@@ -7,14 +7,50 @@ public class StaminaComponent : MonoBehaviour
     [SerializeField]
     private float m_MaxStamina;
     private float m_CurrentStamina;
+    private StaminaUI m_StaminaUI;
+
+    public float CurrentStamina
+    {
+        get
+        {
+            return m_CurrentStamina;
+        }
+        set
+        {
+            m_CurrentStamina = value;
+        }
+        
+        
+    }
+    private void Awake()
+    {
+        m_StaminaUI = FindObjectOfType<StaminaUI>();
+        m_CurrentStamina = m_MaxStamina;
+    }
+
+    
+
+    private void Start()
+    {
+        
+        m_StaminaUI.SetStamina(CurrentStamina);
+    }
+
 
     public void UseStamina(float p_Amount)
     {
-        m_CurrentStamina -= p_Amount;
+        if(m_CurrentStamina > 0)
+        {
+            m_CurrentStamina -= p_Amount;
+            m_StaminaUI.SetStamina(CurrentStamina);
+        }
+        
+
     }
 
     public void GainStamina(float p_Amount)
     {
         m_CurrentStamina += p_Amount;
+        m_StaminaUI.SetStamina(CurrentStamina);
     }
 }
