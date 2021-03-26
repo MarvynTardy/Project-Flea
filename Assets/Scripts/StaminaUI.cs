4da624c639dbd417241664c5138db9316b826c4c
Marvyn Tardy <marvyn.tardy@e-artsup.net>
2021-03-26 15:00:39 +0100

    Correction du bug Script StaminaUI

---
+++ Assets/Scripts/StaminaUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaUI : MonoBehaviour
+{
+    [SerializeField]
+    private Slider m_StaminaAmount;
+    private StaminaComponent m_StaminaComponent;
+    private void Start()
+
+    {
+        m_StaminaAmount = GetComponent<Slider>();
+        m_StaminaComponent = FindObjectOfType<StaminaComponent>();
+        SetStamina(m_StaminaComponent.CurrentStamina);
+    }
+    public void SetStamina(float p_Stamina)
+    {
+        m_StaminaAmount.value = p_Stamina;
+
+    }
+}
