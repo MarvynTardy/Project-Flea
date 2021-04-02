a0fef045c8ebcb5154955f8ef780d33665a93cfe
ArthurCraste <acraste@gmail.com>
2021-04-02 16:56:52 +0200

    Animation event play song

---
+++ Assets/Scripts/LaunchAudioAnimEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchAudioAnimEvent : MonoBehaviour
+{
+    [SerializeField]
+    private string m_EnumName;
+
+    public void LaunchAudio()
+    {
+        AudioManager.Sound l_EnumName = (AudioManager.Sound)System.Enum.Parse(typeof(AudioManager.Sound), m_EnumName);
+        AudioManager.PlaySound(l_EnumName);
+    }
+}
