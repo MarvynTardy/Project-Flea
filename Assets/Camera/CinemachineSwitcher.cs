13cdea1f987d56cffaf348ce896e1f2c58175efb
Marvyn Tardy <marvyn.tardy@e-artsup.net>
2021-04-22 15:22:21 +0200

    Camera Trail WIP

---
+++ Assets/Camera/CinemachineSwitcher.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachineSwitcher : MonoBehaviour
+{
+    private Animator m_Animator;
+    private bool m_ThirdPersonCamera;
+    //private CinemachineBrain m_CinemachineBrain;
+    
+
+    private void Awake()
+    {
+        m_Animator = GetComponent<Animator>();
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            SwitchCamera();
+        }
+        Debug.Log(Camera.current);
+
+        
+    }
+
+    public void SwitchCamera()
+    {
+        
+        m_Animator.Play("CameraRail");
+    }
+    public void ReSwitchCamera()
+    {
+        m_Animator.Play("Main Camera");
+    }
+
+
+   
+}
---
+++ Assets/Camera/TriggerCinemachineSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCinemachineSwitcher : MonoBehaviour
+{
+    private CinemachineSwitcher m_CinemachineSwitcher;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_CinemachineSwitcher.SwitchCamera();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        m_CinemachineSwitcher.ReSwitchCamera();
+    }
+
+}
