03e11c7cca337532b352cb8ff71db0a69ddf173d
Marvyn Tardy <marvyn.tardy@e-artsup.net>
2021-04-22 16:38:39 +0200

    Trigger change cam

--- Assets/Camera/TriggerCinemachineSwitcher.cs
+++ Assets/Camera/TriggerCinemachineSwitcher.cs
@@ -6,9 +6,15 @@ public class TriggerCinemachineSwitcher : MonoBehaviour
 {
     private CinemachineSwitcher m_CinemachineSwitcher;
 
+    private void Start()
+    {
+        m_CinemachineSwitcher = FindObjectOfType<CinemachineSwitcher>();
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Ok");
         m_CinemachineSwitcher.SwitchCamera();
     }
     private void OnTriggerExit(Collider other)
